0e57369a059fabd07f2adf4f6c7b31a90788e9ec
nilzxq <2013358717@qq.com>
2017-02-25 10:39:08 +0800

    method方式

--- HelloWorld/src/com/imooc/action/HelloWorldAction.java
+++ HelloWorld/src/com/imooc/action/HelloWorldAction.java
@@ -1,12 +1,18 @@
 package com.imooc.action;
 
 
+import javax.servlet.http.HttpServletRequest;
+
 import com.opensymphony.xwork2.ActionSupport;
 
-
-
 public class HelloWorldAction extends ActionSupport{
-
+	private HttpServletRequest request;
+	public String add(){
+		return SUCCESS;
+	}
+	public String update(){
+		return SUCCESS;
+	}
 	@Override
 	public String execute() throws Exception {
 		System.out.println("执行Action");
